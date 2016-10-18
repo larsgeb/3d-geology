64164c83ce831d7efd86a5cfa4d999df26e592e5
Lars Gebraad <larsgebraad@gmail.com>
2016-10-19 00:50:20 +0200

    Added (Gulp-)Sass to project; replaced static Skeleton CSS by Skeleton SCSS. Added Gulp-webserver to project, now support live reloading for HTML and CSS (and SCSS) files. Added all libraries and controls from three.js. Updated text on landing page.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,33 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var connect = require('gulp-connect');
 
 gulp.task('test-gulp', function() {
-	console.log('Hello World!');
+  console.log('Hello World!');
+});
+
+gulp.task('sass', function(){
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+    .pipe(connect.reload());
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./scss/**/*.scss', ['sass']);
+});
+
+gulp.task('webserver', function() {
+  connect.server({
+    livereload:true
+  });
+});
+
+gulp.task('html-reload', function(){
+  return gulp.src('*.html').pipe(connect.reload());
+});
+
+gulp.task('default', ['webserver', 'sass'], function() {
+  gulp.watch(['scss/**/*.scss'], ['sass']);
+  gulp.watch(['*.html'],['html-reload']);
 });
